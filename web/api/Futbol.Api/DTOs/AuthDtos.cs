584b6ebd684d1f845f04f0978d1314b385abc0d8
Pepsiee32 <facundo.pezzimenti@gmail.com>
2026-01-06 19:20:11 -0300

    Limpieza: quitando carpetas duplicadas de web

--- web/api/Futbol.Api/DTOs/AuthDtos.cs
+++
@@ -1,5 +0,0 @@
-namespace Futbol.Api.DTOs;
-
-public record RegisterRequest(string Email, string Password);
-public record LoginRequest(string Email, string Password);
-public record MeResponse(string Email);
